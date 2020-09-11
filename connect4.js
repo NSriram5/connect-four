4658e23a0334fcd0a1edb7b9dda690524e7b4f29
Nikhil Sriram <Nikhil.Sriram5@gmail.com>
2020-09-11 15:24:53 -0700

    Minimum functionality established

--- connect4.js
+++ connect4.js
@@ -17,6 +17,19 @@ let board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  // TODO: Complete, tests written
+  board = [];
+  if (WIDTH<0 || HEIGHT<0){
+    console.log("Invalid dimension provided resetting dimensions to 0");
+    WIDTH = 0;
+    HEIGHT = 0;
+  }
+  for (let x = 0; x < WIDTH; x++){
+    board.push([]);
+    for (let y = 0; y<HEIGHT; y++){
+      board[x].push('E');
+    }
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
@@ -28,7 +41,9 @@ function makeHtmlBoard() {
   let htmlBoard = document.getElementById('board');
   
   // TODO: add comment for this code
+  // TODO: Complete
   // This code blocks creates a top row assigns it an id of "column-top" and assigns a click handler to it
+  // Then the code creates a series of cells with an id that corresponds to the column count (left to right)
   let top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
@@ -42,11 +57,13 @@ function makeHtmlBoard() {
   htmlBoard.append(top);
 
   // TODO: add comment for this code
-  for (let y = 0; y < HEIGHT; y++) {
+  // TODO: Complete
+  // This code creates the rest of the game board. The area that game pieces are placed in
+  for (let y = 1; y <= HEIGHT; y++) {
     const row = document.createElement("tr");
     for (let x = 0; x < WIDTH; x++) {
       const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
+      cell.setAttribute("id", `${x}-${HEIGHT - y}`);
       row.append(cell);
     }
     htmlBoard.append(row);
@@ -54,22 +71,36 @@ function makeHtmlBoard() {
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
-
 function findSpotForCol(x) {
+  for (let y = 0; y<HEIGHT;y ++){ 
+    if (board[x][y] === "E"){
+      return y;
+    }
+  }
+  console.log("No spot found to place a piece");
+  return null;
   // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  // TODO: Complete
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
-
-function placeInTable(y, x) {
+function placeInTable(x, y) {
   // TODO: make a div and insert into correct table cell
+  let piece = document.createElement("div");
+  piece.classList.add("piece")
+  piece.classList.add(`Player${currPlayer}`)
+  let target_cell = document.getElementById(`${x}-${y}`)
+  console.log(`${x} - ${y}`);
+  target_cell.append(piece);
+
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  // TODO: Complete
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -81,12 +112,16 @@ function handleClick(evt) {
   // get next spot in column (if none, ignore click)
   let y = findSpotForCol(x);
   if (y === null) {
+    console.log("End of game board reached");
     return;
   }
 
   // place piece in board and add to HTML table
+  placeInTable(x, y);
+
   // TODO: add line to update in-memory board
-  placeInTable(y, x);
+  // TODO: Complete
+  board[x][y] = currPlayer;
 
   // check for win
   if (checkForWin()) {
@@ -95,13 +130,19 @@ function handleClick(evt) {
 
   // check for tie
   // TODO: check if all cells in board are filled; if so call, call endGame
+  // TODO: Complete
+  console.log(board.some((column)=>{column.some(cell => cell === "E")}));
+  if ((board.some((column)=>{column.some(cell => cell === "E")}))){
+    return endGame(`This game has resulted in a tie`);
+  }
 
   // switch players
   // TODO: switch currPlayer 1 <-> 2
-}
+  // TODO: Complete
+  currPlayer===1?currPlayer=2:currPlayer=1;
+  }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
@@ -119,7 +160,7 @@ function checkForWin() {
   }
 
   // TODO: read and understand this code. Add comments to help you.
-
+  // Given a location x,y this code block defines an array of coordinates in each corresponding direction. These arrays are then tested for a win condition using the _win(cell) function (designated private)
   for (let y = 0; y < HEIGHT; y++) {
     for (let x = 0; x < WIDTH; x++) {
       let horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
--- connect4.test.js
+++ connect4.test.js
@@ -8,6 +8,28 @@ describe('Connect Four tests',()=>{
         //feed some test game states to the 
     })
 
+    it("makes a board of width 7 and height 6",()=>{
+        WIDTH = 7;
+        HEIGHT = 6;
+        makeBoard();
+        expect(board.length()).toEqual(WIDTH);
+        expect(board[0].length()).toEqual(HEIGHT);
+    })
+    it ("makes a board of width 0 and height 0",()=>{
+        WIDTH = 0;
+        HEIGHT = 0;
+        makeBoard();
+        expect(board.length()).toEqual(WIDTH);
+        expect(board[0].length()).toEqual(HEIGHT);
+    })
+    it ("makes a board of width -1 and height 0. Will revert to width of 0",()=>{
+        WIDTH = -1;
+        HEIGHT = 6;
+        makeBoard();
+        expect(board.length()).toEqual(WIDTH);
+        expect(board[0].length()).toEqual(HEIGHT);
+    })
+
     afterEach(()=>{
         //teardown commands
     })
