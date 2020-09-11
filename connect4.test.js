ba2f5a91aa7e1b24d3529d3aebc49b0e28d09c58
Nikhil Sriram <Nikhil.Sriram5@gmail.com>
2020-09-11 09:24:34 -0700

    Added testing module

---
+++ connect4.test.js
@@ -0,0 +1,14 @@
+describe('Connect Four tests',()=>{
+    beforeEach(()=>{
+        //game initialization code here
+    })
+    
+    
+    it("returns a boolean if a win is discovered with the player specified",()=>{
+        //feed some test game states to the 
+    })
+
+    afterEach(()=>{
+        //teardown commands
+    })
+})
